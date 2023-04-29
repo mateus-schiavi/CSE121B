4a3f8475bb445ac37dfd59bd90cd9b5e4bfcbe7c
Mateus Schiavi <mtsschiavi@gmail.com>
2023-04-29 12:46:51 -0300

    added pic

--- scripts/task2.js
+++ scripts/task2.js
@@ -3,54 +3,42 @@
 /* VARIABLES */
 
 // Step 1: declare and instantiate a variable to hold your name
-
+const _Name = "Mateus 'Matt' Schiavi";
 
 // Step 2: place the value of the name variable into the HTML file (hint: document.querySelector())
-
+document.querySelector("#name").textContent = _Name;
 
 // Step 3: declare and instantiate a variable to hold the current year
-
+const _currentyear = MMXXIII;
 
 // Step 4: place the value of the current year variable into the HTML file
-
+document.querySelector("#year").textContent = _currentyear;
 
 // Step 5: declare and instantiate a variable to hold the name of your picture
-
+const picName = "images/me.jpg";
 
 // Step 6: copy your image into the "images" folder
 
 // Step 7: place the value of the picture variable into the HTML file (hint: document.querySelector().setAttribute())
-
-
-
+document.querySelector("img").setAttribute("src", picName);
 
 /* ARRAYS */
 
 // Step 1: declare and instantiate an array variable to hold your favorite foods
-
-
+const favFoods = ["Ramen", "Poppy seed chicken", "Chocolate gravy", "Chiken and dumplings"]
 // Step 2: place the values of the favorite foods variable into the HTML file
-
-
+document.querySelector("#food").textContent = favFoods;
 // Step 3: declare and instantiate a variable to hold another favorite food
-
-
+const newFood = "Bean burrito";
 // Step 4: add the variable holding another favorite food to the favorite food array
-
-
+favFoods.push(newFood);
 // Step 5: repeat Step 2
-
-
+document.querySelector("#food").textContent = favFoods;
 // Step 6: remove the first element in the favorite foods array
-
-
+favFoods.splice(0, 1);
 // Step 7: repeat Step 2
-
-
+document.querySelector("#food").textContent = favFoods;
 // Step 8: remove the last element in the favorite foods array
-
-
+favFoods.pop();
 // Step 7: repeat Step 2
-
-
-
+document.querySelector("#food").textContent = favFoods;
