80e1153e3299a8ed2ee95ddccb12f66c028a0b9b
Mateus Schiavi <mtsschiavi@gmail.com>
2023-05-13 15:37:46 -0300

    added task4

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,6 @@ const toggleMenu = () => {
 
 document.querySelector('#toggleMenu').addEventListener('click', toggleMenu);
 
+// set current year in footer
+const currentDate = new Date();
+document.querySelector('#year').textContent = currentDate.getFullYear();
